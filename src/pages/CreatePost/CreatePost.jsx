493cd80fc8a4c2c50181273bc801b9e055eb6eea
giovannimedici <giovannigilsu@gmail.com>
2024-09-12 21:30:12 -0300

    feat: add new post

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,6 +1,9 @@
 import styles from "./CreatePost.module.css";
 
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocuement";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -9,11 +12,51 @@ const CreatePost = () => {
   const [tags, setTags] = useState("");
   const [formError, setFormError] = useState("");
 
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    //validate image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
+    //criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    //checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor preencha todos os campos!");
+    }
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    //redirect to home page
+    navigate("/");
+  };
+
   return (
     <div className={styles.createPost}>
       <h1>Criar Post</h1>
       <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Título: </span>
           <input
@@ -54,12 +97,17 @@ const CreatePost = () => {
             required
             placeholder="Insira as tags separadas por vírgula"
             value={tags}
-            onChange={(e) => setBody(e.target.value)}
+            onChange={(e) => setTags(e.target.value)}
           />
         </label>
-        <button className="btn" type="submit">
-          Cadastrar
-        </button>
+        {!response.loading && <button className="btn">Cadastrar</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
